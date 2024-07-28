88e28087054c1d920fe1728c17b79b97f9f5824d
Cheng <git@zcbenz.com>
2024-07-28 17:15:04 +0900

    Fix ccache cache miss

--- tests/cpp-project.spec.ts
+++ tests/cpp-project.spec.ts
@@ -6,14 +6,15 @@ import {tempDirSync} from '@compilets/using-temp-dir';
 
 import {compileDirectory} from '../src/index.ts';
 
-// We are building tests in temporary directories, without setting this env
-// ccache will not hit cache as it would include cwd in the hash.
+// Do not include cwd in ccache hash, as tests are built in temp dirs.
 process.env.CCACHE_NOHASHDIR = 'true';
+// Do not check compiler time, as we are using downloaded clang.
+process.env.CCACHE_COMPILERCHECK = 'none';
 
 describe('CppProject', () => {
   it('generation', async () => {
     const project = compileDirectory(`${__dirname}/data-cpp-project/noop`);
-    using target = tempDirSync();
+    using target = tempDirSync(`${__dirname}/build-`);
     await project.writeTo(target.path, {generationMode: 'exe'});
     assert.deepStrictEqual(fs.readdirSync(target.path),
                            [ '.gn', 'BUILD.gn', 'cppgc', 'noop.cpp' ]);
